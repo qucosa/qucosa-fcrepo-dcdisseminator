6cf5fabdff6a0aebe66636680d112794c41bc58a
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2017-09-29 16:12:36 +0200

    Reformat
    Reorganize imports, collapse catch staments

--- src/main/java/de/qucosa/dc/disseminator/DcDisseminationServlet.java
+++ src/main/java/de/qucosa/dc/disseminator/DcDisseminationServlet.java
@@ -1,11 +1,12 @@
 package de.qucosa.dc.disseminator;
 
-import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
-import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
-import static javax.servlet.http.HttpServletResponse.SC_OK;
-
-import java.io.IOException;
-import java.net.URI;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -18,46 +19,36 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.net.URI;
 
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
 
 public class DcDisseminationServlet extends HttpServlet {
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
 
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
-	
-    private CloseableHttpClient httpClient;
-    
-    private Transformer transformer;
-    
+    private static final long serialVersionUID = 1L;
     private static final String REQUEST_PARAM_METS_URL = "metsurl";
-	
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private CloseableHttpClient httpClient;
+    private Transformer transformer;
+
     @Override
     public void init() throws ServletException {
         httpClient = HttpClientBuilder.create()
-            .setConnectionManager(new PoolingHttpClientConnectionManager())
-            .build();
-        
+                .setConnectionManager(new PoolingHttpClientConnectionManager())
+                .build();
+
         try {
             transformer = TransformerFactory.newInstance()
-                .newTransformer(new StreamSource(getClass()
-                .getResourceAsStream("/mets2dcdata.xsl")));
+                    .newTransformer(new StreamSource(getClass()
+                            .getResourceAsStream("/mets2dcdata.xsl")));
         } catch (TransformerConfigurationException tce) {
             log.error("Could not initialize XSLT transformer", tce);
             throw new ServletException(tce);
         }
     }
-    
+
     @Override
     public void destroy() {
         try {
@@ -66,55 +57,48 @@ public class DcDisseminationServlet extends HttpServlet {
             log.warn("Problem closing HTTP client: " + e.getMessage());
         }
     }
-	
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        
         try {
             URI metsDocumentUri = URI.create(getRequiredRequestParameterValue(request, REQUEST_PARAM_METS_URL));
-            
+
             try (CloseableHttpResponse resp = httpClient.execute(new HttpGet(metsDocumentUri))) {
-                
+
                 if (SC_OK == resp.getStatusLine().getStatusCode()) {
                     transformer.transform(
-                        new StreamSource(resp.getEntity().getContent()),
-                        new StreamResult(response.getOutputStream())
+                            new StreamSource(resp.getEntity().getContent()),
+                            new StreamResult(response.getOutputStream())
                     );
                     response.setStatus(SC_OK);
                 }
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (UnsupportedOperationException e) {
-                e.printStackTrace();
+
             } catch (TransformerException e) {
-                e.printStackTrace();
+                log.error("Error transforming METS/MODS: " + e.getMessage());
+                sendError(response, SC_INTERNAL_SERVER_ERROR, e.getMessage());
             }
-        } catch (MissingRequiredParameter  | IllegalArgumentException mrp) {
-            sendError(response, SC_BAD_REQUEST, mrp.getMessage());
-            mrp.printStackTrace();
+
         } catch (Throwable anythingElse) {
             log.warn("Internal server error", anythingElse);
             sendError(response, SC_INTERNAL_SERVER_ERROR, anythingElse.getMessage());
         }
     }
-    
+
     private void sendError(HttpServletResponse resp, int status, String msg) throws IOException {
         resp.setStatus(status);
         resp.setContentType("text/plain");
         resp.setContentLength(msg.getBytes().length);
         resp.getWriter().print(msg);
     }
-    
+
     private String getRequiredRequestParameterValue(ServletRequest request, String param)
             throws MissingRequiredParameter {
         final String value = request.getParameter(param);
-        
+
         if (value == null || value.isEmpty()) {
             throw new MissingRequiredParameter("Missing parameter '" + REQUEST_PARAM_METS_URL + "'");
         }
-        
+
         return value;
     }
 }
