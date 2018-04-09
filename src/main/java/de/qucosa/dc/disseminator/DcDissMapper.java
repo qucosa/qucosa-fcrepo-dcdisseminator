8392864792028054751354cc731c00605f52e065
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2018-04-09 12:23:47 +0200

    Reformatting

--- src/main/java/de/qucosa/dc/disseminator/DcDissMapper.java
+++ src/main/java/de/qucosa/dc/disseminator/DcDissMapper.java
@@ -1,8 +1,6 @@
 package de.qucosa.dc.disseminator;
 
-import java.io.ByteArrayInputStream;
-import java.io.StringWriter;
-import java.util.HashMap;
+import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,19 +14,20 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Document;
+import java.io.ByteArrayInputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
 
 public class DcDissMapper {
     private Document metsDoc = null;
 
     private StreamSource xslSource = null;
-    
+
     public DcDissMapper(Document metsDoc, StreamSource xslSource) {
         this.metsDoc = metsDoc;
         this.xslSource = xslSource;
     }
-    
+
     public Document transformDcDiss() throws TransformerFactoryConfigurationError, Exception, XPathExpressionException {
         Transformer transformer = null;
         StringWriter stringWriter = new StringWriter();
@@ -44,22 +43,22 @@ public class DcDissMapper {
 
         return xmetadiss;
     }
-    
+
     public String pid() throws XPathExpressionException {
         return extractPid();
     }
-    
+
     public String lastModeDate() throws XPathExpressionException {
         return extractLastModDate();
     }
-    
+
     private String extractPid() throws XPathExpressionException {
         String pid = null;
         XPath xPath = xpath();
         pid = (String) xPath.compile("//mets:mets/@OBJID").evaluate(metsDoc, XPathConstants.STRING);
         return pid;
     }
-    
+
     private String extractLastModDate() throws XPathExpressionException {
         String date = null;
         XPath xPath = xpath();
